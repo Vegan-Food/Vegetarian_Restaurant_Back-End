659bb92ef3181768b921a5bd143689d70ebc2dfa
Your Name <you@example.com>
2025-06-23 13:10:51 +0700

    swagger

--- src/main/java/com/veganfood/veganfoodbackend/controller/UserController.java
+++ src/main/java/com/veganfood/veganfoodbackend/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.veganfood.veganfoodbackend.controller;
+import com.veganfood.veganfoodbackend.model.User;
+import com.veganfood.veganfoodbackend.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
